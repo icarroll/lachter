caa8757f8ea41531c43a92d3e4679011cedf5aa6
Isaac Carroll <icarroll@pobox.com>
2018-12-12 20:27:51 -0800

    work on game state validation

--- lachter.cc
+++ lachter.cc
@@ -1,5 +1,58 @@
 #include "lachter.hh"
 
-int main(int numargs, char * args[]) {
-    return 0;
+using namespace std;
+
+bool inbounds(coord pos) {
+    return 0 <= pos.x && pos.x < SIZE && 0 <= pos.y && pos.y < SIZE;
 }
+
+bool gamestate::legal() {
+    boardmap occupied = blocks;
+
+    // all pieces at legal coords
+    // no pieces at same coords
+    int dwarfcount = 0;
+    for (int ix=0 ; ix<MAX_DWARFS ; ix+=1) {
+        piecestate dwarf = dwarfs[ix];
+        if (! dwarf.alive) continue;
+        coord pos = dwarf.pos;
+        if (! inbounds(pos)) return false;
+        if (occupied[pos.y][pos.x]) return false;
+        occupied[pos.y][pos.x] = true;
+        dwarfcount += 1;
+    }
+
+    int trollcount = 0;
+    for (int ix=0 ; ix<MAX_TROLLS ; ix+=1) {
+        piecestate troll = trolls[ix];
+        if (! troll.alive) continue;
+        coord pos = troll.pos;
+        if (! inbounds(pos)) return false;
+        if (occupied[pos.y][pos.x]) return false;
+        occupied[pos.y][pos.x] = true;
+        trollcount += 1;
+    }
+
+    // piece counts accurate
+    if (dwarfcount != numdwarfs || trollcount != numtrolls) return false;
+
+    // dwarf mobility accurate
+    for (int ix=0 ; ix<MAX_DWARFS ; ix+=1) {
+        piecestate dwarf = dwarfs[ix];
+        if (! dwarf.alive) {
+            dwarfmobility[ix] = {};
+            continue;
+        }
+        coord pos = dwarf.pos;
+    }
+
+    // threat maps accurate
+}
+
+/*
+vector<gamemove> gamestate::allmoves() {
+    return vector<gamemove>();
+
+    return moves;
+}
+*/
--- lachter.hh
+++ lachter.hh
@@ -1,26 +1,69 @@
 #pragma once
 
-struct piecestate {
-    bool alive;
+#include <array>
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+struct coord {
     int x;
     int y;
+};
+
+const int NUM_DIRS = 8;
+
+array<coord,NUM_DIRS> dirs = {(coord){1,0},  {1,1},   {0,1},  {-1,1},
+                                     {-1,0}, {-1,-1}, {0,-1}, {1,-1}};
+
+struct piecestate {
+    bool alive;
+    coord pos;
     bool inthreat;
+};
+
+const int SIZE = 15;
+
+typedef array<array<bool,SIZE>,SIZE> boardmap;
+
+const boardmap blocks = {1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,
+                         1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,
+                         1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,
+                         1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,
+                         1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                         0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,
+                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                         1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                         1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,
+                         1,1,1,0,0,0,0,0,0,0,0,0,1,1,1,
+                         1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,
+                         1,1,1,1,1,0,0,0,0,0,1,1,1,1,1};
+
+struct gamemove {
+    bool isdwarfmove;
+    coord from;
+    coord to;
+    bool capt;
+    uint8_t capts;
 };
 
 const int MAX_DWARFS = 32;
 const int MAX_TROLLS = 8;
-const int NUM_DIRS = 8;
-const int SIZE = 15;
-
-typedef bool threatmap[SIZE][SIZE];
 
 struct gamestate {
     bool isdwarfturn;
-    piecestate dwarfs[MAX_DWARFS];
-    piecestate trolls[MAX_TROLLS];
+    array<piecestate,MAX_DWARFS> dwarfs;
+    array<piecestate,MAX_TROLLS> trolls;
     int numdwarfs;
     int numtrolls;
-    int mobility[MAX_DWARFS][NUM_DIRS];
-    threatmap dwarfthreats;
-    threatmap trollthreats;
+    array<array<int,NUM_DIRS>,MAX_DWARFS> dwarfmobility;
+    boardmap dwarfthreats;
+    boardmap trollthreats;
+
+    bool legal();
+    //vector<gamemove> allmoves();
 };
