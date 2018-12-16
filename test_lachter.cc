4b5f0ebbb6834e844181f1a413128f759a0ed77e
Isaac Carroll <icarroll@pobox.com>
2018-12-16 14:28:52 -0800

    optimize, randomize properly

--- test_lachter.cc
+++ test_lachter.cc
@@ -4,16 +4,16 @@
 #include "lachter.hh"
 
 int main(int numargs, char * args[]) {
-    mt19937 gen(0);
+    mt19937 gen(time(NULL));
 
-    for (int tries=0 ; tries<1000 ; tries+=1) {
+    for (int tries=0 ; tries<100000 ; tries+=1) {
         gamestate board;
         if (! board.valid()) return 1;
 
         vector<gamemove> played = {};
         while (! board.gameover()) {
             vector<gamemove> allmoves = board.allmoves();
-            uniform_int_distribution<> dis(0, allmoves.size());
+            uniform_int_distribution<> dis(0, allmoves.size()-1);
             int ix = dis(gen);
             played.push_back(allmoves[ix]);
             board.domove(allmoves[ix]);
